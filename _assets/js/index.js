3a23e8364ffd9e3d2a31a7d1b3d9445452d64cfc
Naitian Zhou <nzhou2011@gmail.com>
2017-01-07 13:31:50 -0500

    Start Work on Terminal

--- _assets/js/index.js
+++ _assets/js/index.js
@@ -4,9 +4,9 @@ window.onload = () => {
     flip.addEventListener('click', (e) => {
         const body = document.getElementsByTagName('body')[0];
         const term = new Terminal(body);
-        flip.style.borderLeft = '200vw solid #fff0af';
+        flip.style.borderLeft = '200vw solid #92a8da';
         flip.style.borderBottom = '200vh solid #272727';
 
         flip.addEventListener('transitionend', term.init);
-    })
-}
+    });
+};
