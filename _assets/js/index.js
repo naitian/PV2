d78b7bab76ed606d20cada27c1e9dc3ade88d34b
Naitian Zhou <nzhou2011@gmail.com>
2017-03-06 22:31:35 -0500

    Fix browser history stuff

--- _assets/js/index.js
+++ _assets/js/index.js
@@ -3,11 +3,27 @@
 window.onload = () => {
     const flip = document.querySelector('#flip');
     flip.addEventListener('click', () => {
-        const body = document.getElementsByTagName('body')[0];
-        const term = new Terminal(body);
-        flip.style.borderLeft = '200vw solid #92a8da';
-        flip.style.borderBottom = '200vh solid #272727';
-
-        flip.addEventListener('transitionend', term.init);
+      // activateShell();
+      window.location.hash = '#terminal';
+      window.location.reload(true) 
     });
+
+    if (window.location.hash === '#terminal') {
+      activateShell();
+    }
 };
+
+window.onhashchange = () => {
+  window.location.reload(true)
+}
+
+let activateShell = () => {
+
+  const body = document.getElementsByTagName('body')[0];
+  const term = new Terminal(body);
+  flip.style.borderLeft = '200vw solid #92a8da';
+  flip.style.borderBottom = '200vh solid #272727';
+
+  flip.addEventListener('transitionend', term.init);
+
+}
